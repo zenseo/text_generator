896279ce9dcfb565ef9fbaf09b80d12148bf35a7
Rugoals <rugoals@gmail.com>
2014-11-03 19:33:29 +0300

    удалил старые названия переменных

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <?php
 header('Content-type: text/html; charset=utf-8');
-setlocale(LC_ALL, 'ru_RU.UTF-8'); 
 
+setlocale(LC_ALL, 'ru_RU.UTF-8'); 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
  
 include_once($_SERVER['DOCUMENT_ROOT']."/text_generator/classes/tg.class.php");
-$stg = new tg();
+$tg = new tg();
  
-echo $stg->run(
-	'title.txt',
-	$url = '',
-	array(
-		'name' => 'Test'
-	)
+$tg->run( 
+	$text = 'test.txt', 
+	$url = array(), 
+	$use_vars = array( 'name' => 'Test' ) 
 );
